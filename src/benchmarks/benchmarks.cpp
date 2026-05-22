f3a4cb4983675c21011ce5a9912b85f1fb463cfd
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2026-05-23 00:56:02 +0300

    Implement high zoom benchmarks

--- src/benchmarks/benchmarks.cpp
+++ src/benchmarks/benchmarks.cpp
@@ -5,6 +5,9 @@
 #include "mandelbrot/core/cuda/mandelbrot_set.h"
 #include "mandelbrot/core/image.h"
 #include "mandelbrot/core/palette.h"
+#include <mandelbrot/core/utils.h>
+
+#include <charconv>
 
 namespace mandelbrot {
 
@@ -88,6 +91,65 @@ BENCHMARK(DifferentMaxIterations)
   ->Arg(4096)
   ->Arg(8192)
   ->MinTime(1)
+  ->Unit(benchmark::kMillisecond);
+
+template <typename Float>
+static Float InitFloat(std::string_view literal) {
+  if constexpr (std::is_same_v<Float, double_t>) {
+    auto v = double_t{};
+    std::from_chars(literal.data(), literal.data() + literal.size(), v);
+    return v;
+  } else {
+    return Float(std::string(literal));
+  }
+}
+
+static void HighZoomNormal(benchmark::State& state) {
+  constexpr auto kSize = Size{ 1024, 768 };
+  constexpr auto kColoringMode = static_cast<uint32_t>(ColoringMode::kMode1);
+  constexpr auto kPalette = static_cast<uint32_t>(Palette::kBluePalette);
+  constexpr auto kSmoothing = true;
+
+  const auto real = InitFloat<Float>("-0.2294379204917521921283938659935972297779995325625177334975946836024825031811734391974523087417743062020561864658849527129599780000");
+  const auto imag = InitFloat<Float>("+0.8209092363709344023306248042908969588444321104496672178513855603915842881211531455988556895799006496899604886990696093819533990000");
+  const auto zoom = InitFloat<Float>("8617594940382931955969294001603998395666896884150973047658560539684745901643990851155924198047262999813551579129376683.12893800950000");
+  const auto max_iterations = 4096;
+
+  auto image = Image{ kSize };
+
+  for (auto _ : state) {
+    cuda::VisualizePerturbation(image.GetData(), image.GetWidth(), image.GetHeight(),
+      real, imag, zoom, max_iterations,
+      kColoringMode, kPalette, kSmoothing);
+  }
+}
+
+BENCHMARK(HighZoomNormal)
+  ->Iterations(5)
+  ->Unit(benchmark::kMillisecond);
+
+static void HighZoomHeavy(benchmark::State& state) {
+  constexpr auto kSize = Size{ 1024, 768 };
+  constexpr auto kColoringMode = static_cast<uint32_t>(ColoringMode::kMode1);
+  constexpr auto kPalette = static_cast<uint32_t>(Palette::kBluePalette);
+  constexpr auto kSmoothing = true;
+
+  const auto real = InitFloat<Float>("-0.7499962814923143790642160359331255832889316887107532993957730552496709780558365928185421048044863443589735578622619681905794452100");
+  const auto imag = InitFloat<Float>("-0.0065511461630430211865759781060440814464714161394296919967918757588058752769649158251142223443778126729329264578443902386756515482");
+  const auto zoom = InitFloat<Float>("494674475186802996536021892450697064199.575524610587356084897548405961134504813639661199849287243076131546113144383715927074969970000");
+  const auto max_iterations = 8192;
+
+  auto image = Image{ kSize };
+
+  for (auto _ : state) {
+    cuda::VisualizePerturbation(image.GetData(), image.GetWidth(), image.GetHeight(),
+      real, imag, zoom, max_iterations,
+      kColoringMode, kPalette, kSmoothing);
+  }
+}
+
+BENCHMARK(HighZoomHeavy)
+  ->Iterations(5)
   ->Unit(benchmark::kMillisecond);
 
 }
